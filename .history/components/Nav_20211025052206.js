e211339127672a81d6282ce0319a8624648854b7
realisticattorney <g.axl.aquila@gmail.com>
2021-10-25 05:24:10 -0300

    Add nav component

--- .history/components/Nav_20211025052206.js
+++ .history/components/Nav_20211025052206.js
@@ -4,12 +4,7 @@ const Nav = () => {
   return (
     <header className="border-b sticky top-0 z-20 bg-white">
       <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 mx-auto"></div>
-      <Link href="/" passHref>
-        <a className="cursor-pointer">
-          <span className="text-lg pt-1 font-bold">deepoe</span>
-        </a>
-      </Link>
-      <a className="text-md font-bold cursor-pointer">Cart</a>
+      <Link href="/"></Link>
     </header>
   );
 };
--- .history/components/Nav_20211025052345.js
+++ .history/components/Nav_20211025052345.js
@@ -5,11 +5,15 @@ const Nav = () => {
     <header className="border-b sticky top-0 z-20 bg-white">
       <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 mx-auto"></div>
       <Link href="/" passHref>
-        <a className="cursor-pointer">
-          <span className="text-lg pt-1 font-bold">deepoe</span>
-        </a>
+         <a className="cursor-pointer">
+            <span className="text-lg pt-1 font-bold">
+               deepoe
+            </span>
+         </a>
       </Link>
-      <a className="text-md font-bold cursor-pointer">Cart</a>
+      <a className="text-md font-bold ">
+         Cart
+      </a>
     </header>
   );
 };

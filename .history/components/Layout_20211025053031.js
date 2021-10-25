82880665944990b727de1216241809642287d79c
realisticattorney <g.axl.aquila@gmail.com>
2021-10-25 05:32:14 -0300

    Add dummy footer

--- .history/components/Layout_20211025053031.js
+++ .history/components/Layout_20211025053031.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
     <div className="flex flex-col justify-between min-h-screen">
       <Nav />
       <main>{children}</main>
-      <footer className="bg-gray-700 text-white text-center">Footer</footer>
+      
     </div>
   );
 };

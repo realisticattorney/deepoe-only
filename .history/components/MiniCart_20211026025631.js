97f93bf5dfb5ac87cd0dcdfdaf47a20b10ee031c
realisticattorney <g.axl.aquila@gmail.com>
2021-10-26 02:58:30 -0300

    Add conditional to sum total

--- .history/components/MiniCart_20211026025631.js
+++ .history/components/MiniCart_20211026025631.js
@@ -85,9 +85,7 @@ export default function MiniCart({ cart }) {
                                     <h3>
                                       <a href={product.href}>{product.title}</a>
                                     </h3>
-                                    <p className="ml-4">
-                                      {formatter.format(product.variantPrice)}
-                                    </p>
+                                    <p className="ml-4">{formatter.format(product.variantPrice)}</p>
                                   </div>
                                   <p className="mt-1 text-sm text-gray-500">
                                     {product.variantTitle}
@@ -115,38 +113,37 @@ export default function MiniCart({ cart }) {
                     </div>
                   </div>
 
-                  {cart.length > 0 ? (
-                    <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
-                      <div className="flex justify-between text-base font-medium text-gray-900">
-                        <p>Subtotal</p>
-                        <p>{formatter.format(cartTotal)}</p>
-                      </div>
-                      <p className="mt-0.5 text-sm text-gray-500">
-                        Shipping and taxes calculated at checkout.
-                      </p>
-                      <div className="mt-6">
-                        <a
-                          href="#"
-                          className="flex justify-center items-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                        >
-                          Checkout
-                        </a>
-                      </div>
-                      <div className="mt-6 flex justify-center text-sm text-center text-gray-500">
-                        <p>
-                          or{' '}
-                          <button
-                            type="button"
-                            className="text-indigo-600 font-medium hover:text-indigo-500"
-                            onClick={() => setOpen(false)}
-                          >
-                            Continue Shopping
-                            <span aria-hidden="true"> &rarr;</span>
-                          </button>
-                        </p>
-                      </div>
+               
+                  <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+                    <div className="flex justify-between text-base font-medium text-gray-900">
+                      <p>Subtotal</p>
+                      <p>{}</p>
                     </div>
-                  ) : null}
+                    <p className="mt-0.5 text-sm text-gray-500">
+                      Shipping and taxes calculated at checkout.
+                    </p>
+                    <div className="mt-6">
+                      <a
+                        href="#"
+                        className="flex justify-center items-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                      >
+                        Checkout
+                      </a>
+                    </div>
+                    <div className="mt-6 flex justify-center text-sm text-center text-gray-500">
+                      <p>
+                        or{' '}
+                        <button
+                          type="button"
+                          className="text-indigo-600 font-medium hover:text-indigo-500"
+                          onClick={() => setOpen(false)}
+                        >
+                          Continue Shopping
+                          <span aria-hidden="true"> &rarr;</span>
+                        </button>
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </Transition.Child>

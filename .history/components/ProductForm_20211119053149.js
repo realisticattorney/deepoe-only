70bcdd4f91fb58e1aa649da82b323cc64791ad8c
realisticattorney <g.axl.aquila@gmail.com>
2021-11-19 05:32:41 -0300

    Add conditional for Final sale no returns disclaimer

--- .history/components/ProductForm_20211119053149.js
+++ .history/components/ProductForm_20211119053149.js
@@ -115,15 +115,14 @@ const ProductForm = ({ product }) => {
         ))}
         <div className="text-sm font-light mb-2.5 mt-0.5 space-y-1.5">
           <p className="flex items-center text-xs">
-            <ExclamationCircleIcon className="h-4 flex pr-1" /> Usually ships in
-            1-2 weeks
+            <ExclamationCircleIcon className="h-4 flex pr-1" /> Usually ships in 1-2
+            weeks
           </p>
           {product.variants.edges[0].node.compareAtPriceV2?.amount && (
-            <p className="flex items-center text-xs">
-              <TruckIcon className="h-4 flex pr-1" />
-              Final Sale! No Returns
-            </p>
           )}
+          <p className="flex items-center text-xs">
+            <TruckIcon className="h-4 flex pr-1" />Final Sale! No Returns
+          </p>
         </div>
         <button
           onClick={() => {

ef2efe99de8de1805a69971cb08e1e68be237e04
realisticattorney <g.axl.aquila@gmail.com>
2021-12-07 18:05:28 -0300

    Fix width for product list

---
+++ .history/components/ProductList_20211207174152.js
@@ -0,0 +1,67 @@
+import ProductCard from './ProductCard';
+import Image from 'next/image';
+const ProductList = ({ collection, products }) => {
+  return (
+    <div className="w-full  overflow-hidden">
+    <div className="relative h-65">
+        <Image
+          src={products.image.originalSrc}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          
+        />
+       <h2
+          className={`text-3xl md:text-4xl text-white absolute left-0 right-0 ml-auto mr-auto top-16 italic font-medium w-80 md:w-96 mb-6 text-center 
+            `}
+        >
+          Considered materials, lively design.
+        </h2>
+        <a
+          href={`https://deepoe-only.vercel.app/collections/frontpage`}
+          rel="noreferrer"
+        >
+          <p className="text-white text-sm absolute  font-light text-center right-0 mr-auto mb-6 bottom-0 left-0 ml-auto w-40 underline font-mono">
+            Shop All Products
+          </p>
+        </a>
+      </div>
+      <div className="px-6 my-6 w-full relative md:w-150 md:mx-auto">
+        <Image
+          src="/deepoeHome3.jpg"
+          layout="responsive"
+          width={378}
+          height={284}
+          quality={100}
+          objectFit="contain"
+        />
+      </div>
+      <div className="px-4 pt-6 ">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6">
+          {products.products.edges.map((product) => (
+            <ProductCard
+              key={product.node.id}
+              product={product}
+              collection={collection}
+            />
+          ))}
+        </div>
+        <div className="py-6"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
+
+// ${
+//   collection.length === 10 ||
+//   (collection.length === 11 && 'left-18')
+// } ${collection.length === 9 && 'left-16'}
+//  ${collection.length === 4 && 'left-40'} ${
+//   collection.length === 8 && 'left-24'
+// }
+// ${
+//   collection === 'dinnerware' && 'left-24'
+// }
+//  ${collection.length === 7 && 'left-24'}`
---
+++ .history/pages/collections/[collection]/index_20211207175855.js
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import { getCollections, getProductsInCollection } from '../../../lib/shopify';
+import ProductList from '../../../components/ProductList';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+export default function Collection({ collection, products }) {
+  // console.log('collection223123123', products);
+  return (
+    <div className="xl:m">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ProductList products={products} collection={collection} />
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const collections = await getCollections();
+  const paths = collections.map((collection) => ({
+    params: { collection: String(collection.node.handle) },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  // console.log('params collection', params);
+  const products = await getProductsInCollection(params.collection);
+  console.log("get products in collection", products);
+  return {
+    props: {
+      collection: params.collection,
+      products,
+    },
+  };
+}

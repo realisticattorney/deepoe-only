fe09060bf16dfe40e0b1afcd5833832bedd408a3
realisticattorney <g.axl.aquila@gmail.com>
2021-12-12 01:25:29 -0300

    Add redirect

--- .history/next.config_20211119182738.js
+++ .history/next.config_20211119182738.js
@@ -1,13 +1,4 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ];
-  },
   env: {
     SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
     SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,

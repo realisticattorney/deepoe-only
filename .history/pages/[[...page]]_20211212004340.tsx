7be7756632641e809f0bbbdbc7d3415f447e4f93
realisticattorney <g.axl.aquila@gmail.com>
2021-12-12 01:23:08 -0300

    Add [[page]].tsx

---
+++ .history/pages/[[...page]]_20211212004340.tsx
@@ -0,0 +1,83 @@
+// import Head from 'next/head';
+// import { getProductsInCollection } from '../lib/shopify';
+// import ProductList from '../components/ProductList';
+// import HomeCarousel from '../components/HomeCarousel';
+// import Layout from '../components/Layout';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { useState } from 'react';
+// import HomeMain from '../components/HomeMain';
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
+import { useRouter } from 'next/router';
+import { BuilderComponent, Builder, builder } from '@builder.io/react';
+import DefaultErrorPage from 'next/error';
+import Head from 'next/head';
+import '../components/HomeMain';
+import '../components/FaqSection';
+
+const BUILDER_API_KEY = '4a1ff4a5cfa24cfe9eaaddc710a33a27';
+builder.init(BUILDER_API_KEY);
+
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext<{ page: string[] }>) {
+  const page =
+    (await builder
+      .get('page', {
+        userAttributes: {
+          urlPath: '/' + (params?.page?.join('/') || ''),
+        },
+      })
+      .toPromise()) || null;
+
+  return {
+    props: {
+      page,
+    },
+    revalidate: 5,
+  };
+}
+
+export async function getStaticPaths() {
+  const pages = await builder.getAll('page', {
+    options: { noTargeting: true },
+  });
+
+  return {
+    paths: pages.map((page) => `${page.data?.url}`),
+    fallback: true,
+  };
+}
+
+export default function Page({
+  page,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+  const isLive = !Builder.isEditing && !Builder.isPreviewing;
+  if (!page && isLive) {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+          <meta name="title"></meta>
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    );
+  }
+
+  console.log("ROOOUTER", router);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <BuilderComponent model="page" content={page} />
+    </>
+  );
+}

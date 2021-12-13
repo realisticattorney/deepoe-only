3a66f8bbbeb4d06f778ec06ce02724c2f72f017a
realisticattorney <g.axl.aquila@gmail.com>
2021-12-13 03:14:03 -0300

    Add hear icon svg to product page

--- .history/components/ProductForm_20211213030150.js
+++ .history/components/ProductForm_20211213030150.js
@@ -192,7 +192,7 @@ const ProductForm = ({ product, collection }) => {
             className={`  px-2 pt-2 pb-1.5 mt-1 mb-3 text-2xl font-sans self-center justify-self-center items-center  w-full active:scale-95 ${
               selectedVariant.availableForSale
                 ? 'disabled bg-deepoe-chocolate text-white'
-                : 'text-deepoe-chocolate border border-deepoe-chocolate bg-white font-medium focus:outline-none'
+                : 'text-deepoe-chocolate border border-deepoe-chocolate bg-white font-medium focus:outline-none '
             }`}
             disabled={selectedVariant.availableForSale === false ? true : false}
           >

345e8ca5e288f43a4490894c72fd146f6a328d33
realisticattorney <g.axl.aquila@gmail.com>
2021-12-14 04:58:17 -0300

    Refactor collections page props type

--- .history/pages/collections/index_20211214045714.tsx
+++ .history/pages/collections/index_20211214045714.tsx
@@ -3,6 +3,40 @@ import { getCollections } from '../../lib/shopify';
 import CollectionList from '../../components/CollectionList';
 import type { InferGetStaticPropsType } from 'next';
 
+interface AllCollections {
+  allCollections: Collection[];
+}
+
+export interface Collection {
+  node: {
+    description: string;
+    handle: string;
+    id: string;
+    title: string;
+    image: {
+      id: string;
+      originalSrc: string;
+    };
+    products: {
+      edges: [
+        {
+          node: {
+            images: {
+              edges: [
+                {
+                  node: {
+                    originalSrc: string;
+                  };
+                }
+              ];
+            };
+          };
+        }
+      ];
+    };
+  };
+}
+
 const AllCollections = ({
   allCollections,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {

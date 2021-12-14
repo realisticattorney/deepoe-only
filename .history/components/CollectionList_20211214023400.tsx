56b61a618873bb0e3282ffee2c2af6b35235fe98
realisticattorney <g.axl.aquila@gmail.com>
2021-12-14 02:53:57 -0300

    Fix bug with passing props from CollectionList to CollectionCard

---
+++ .history/components/CollectionList_20211214023400.tsx
@@ -0,0 +1,50 @@
+import CollectionCard from './CollectionCard';
+
+interface AllCollections {
+  allCollections: Collection[];
+}
+
+export interface Collection {
+  node: {
+    id: string;
+    handle: string;
+    title: string;
+    description: string;
+    image: {
+      id: string;
+      originalSrc: string;
+    };
+    products: {
+      edges: {
+        node: {
+          images: {
+            edges: {
+              node: {
+                originalSrc: string;
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
+const ProductList = ({ allCollections }: AllCollections) => {
+  return (
+    <div className="bg-deepoe-cream">
+      <div className="max-w-2xl mx-auto py-6 px-4">
+        <h2 className="text-2xl font-extrabold text-gray-800 mb-6">
+          Collections
+        </h2>
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6">
+          {allCollections.map((collection) => (
+            <CollectionCard:  key={collection.node.id} collection={collection} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;

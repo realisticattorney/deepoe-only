062ffb7273ee2d0f539fa2853ff3eeb35d2c3195
realisticattorney <g.axl.aquila@gmail.com>
2021-12-15 08:03:56 -0300

    Migrate to js

---
+++ .history/pages/collections/index_20211215080342.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { getCollections } from '../../lib/shopify';
+import CollectionList from '../../components/CollectionList';
+import type { InferGetStaticPropsType } from 'next';
+
+const AllCollections = ({ allCollections }) => {
+  return (
+    <div>
+      <CollectionList allCollections={allCollections} />
+    </div>
+  );
+};
+
+export default AllCollections;
+
+export async function getStaticProps() {
+  const allCollections = await getCollections();
+
+  return {
+    props: { allCollections },
+  };
+}
---
+++ pages/collections/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { getCollections } from '../../lib/shopify';
+import CollectionList from '../../components/CollectionList';
+
+const AllCollections = ({ allCollections }) => {
+  return (
+    <div>
+      <CollectionList allCollections={allCollections} />
+    </div>
+  );
+};
+
+export default AllCollections;
+
+export async function getStaticProps() {
+  const allCollections = await getCollections();
+
+  return {
+    props: { allCollections },
+  };
+}
